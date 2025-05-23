e1106c977d09b177f3da052af8f49b02f5f9f0c9
Yerdeee <154613415+Yerdeee@users.noreply.github.com>
2025-05-23 13:09:26 +0000

    ecg_clean 수정 및 변수 이름 조정

--- data.py
+++ data.py
@@ -6,23 +6,30 @@ import sklearn.preprocessing as skp
 from torch.utils.data import Dataset, DataLoader
 
 class ECGDataset(Dataset):
-    def __init__(self, ecg_data, ppg_data):
-        self.ecg_data = ecg_data
-        self.ppg_data = ppg_data
+    def __init__(self, ecgall_data, ecgcond_data, with_ecgclean):
+        self.ecgall_data = ecgall_data
+        self.ecgcond_data = ecgcond_data
+        self.with_ecgclean = with_ecgclean
 
     def __getitem__(self, index):
 
-        ecg = self.ecg_data[index]
-        ppg = self.ppg_data[index]
+        ecgall = self.ecgall_data[index]
+        ecgcond = self.ecgcond_data[index]
         
-        window_size = ecg.shape[-1]
+        window_size = ecgall.shape[-1]
 
-        ppg = nk.ppg_clean(ppg.reshape(window_size), sampling_rate=128)
-        ecg = nk.ecg_clean(ecg.reshape(window_size), sampling_rate=128, method="pantompkins1985")
-        _, info = nk.ecg_peaks(ecg, sampling_rate=128, method="pantompkins1985", correct_artifacts=True, show=False)
+        if self.with_ecgclean == True :
+            ecgall = nk.ecg_clean(ecgall.reshape(window_size), sampling_rate=128, method="pantompkins1985")
+            ecgcond = nk.ecg_clean(ecgcond.reshape(window_size), sampling_rate=128, method="pantompkins1985")
+
+        else :
+            ecgall = ecgall.reshape(window_size)
+            ecgcond = ecgcond.reshape(window_size)
+
+        _, info = nk.ecg_peaks(ecgall, sampling_rate=128, method="pantompkins1985", correct_artifacts=True, show=False)
 
         # Create a numpy array for ROI regions with the same shape as ECG
-        ecg_roi_array = np.zeros_like(ecg.reshape(1, window_size))
+        ecg_roi_array = np.zeros_like(ecgall.reshape(1, window_size))
 
         # Iterate through ECG R peaks and set values to 1 within the ROI regions
         roi_size = 32
@@ -31,62 +38,52 @@ class ECGDataset(Dataset):
             roi_end = min(roi_start + roi_size, window_size)
             ecg_roi_array[0, roi_start:roi_end] = 1
 
-        return ecg.reshape(1, window_size).copy(), ppg.reshape(1, window_size).copy(), ecg_roi_array.copy() #, ppg_cwt.copy()
+        return ecgall.reshape(1, window_size).copy(), ecgcond.reshape(1, window_size).copy(), ecg_roi_array.copy() #, ppg_cwt.copy()
 
     def __len__(self):
-        return len(self.ecg_data)
+        return len(self.ecgall_data)
 
 def get_datasets(
-    DATA_PATH = "/tf/yj/ppg2ecg/datasets/", 
-    #datasets=["BIDMC", "CAPNO", "DALIA", "MIMIC-AFib", "WESAD"],
-    datasets=["PTBXL"],
-    window_size=4,
+    DATA_PATH = "/tf/hsh/SW_ECG/single_lead_data/",
+    datasets=[""],
+    window_size=5,
+    with_ecgclean=True
     ):
 
-    ecg_train_list = []
-    ppg_train_list = []
-    ecg_test_list = []
-    ppg_test_list = []
+    ecgall_train_list = []
+    ecgcond_train_list = []
+    ecgall_test_list = []
+    ecgcond_test_list = []
     
-    # for dataset in datasets:
-    #     print(dataset)
-    #     ecg_train = np.load(DATA_PATH + dataset + f"/ecg_train_{window_size}sec.npy", allow_pickle=True).reshape(-1, 128*window_size)
-    #     ppg_train = np.load(DATA_PATH + dataset + f"/ppg_train_{window_size}sec.npy", allow_pickle=True).reshape(-1, 128*window_size)
-        
-    #     ecg_test = np.load(DATA_PATH + dataset + f"/ecg_test_{window_size}sec.npy", allow_pickle=True).reshape(-1, 128*window_size)
-    #     ppg_test = np.load(DATA_PATH + dataset + f"/ppg_test_{window_size}sec.npy", allow_pickle=True).reshape(-1, 128*window_size)
-
-    #     ecg_train_list.append(ecg_train)
-    #     ppg_train_list.append(ppg_train)
-    #     ecg_test_list.append(ecg_test)
-    #     ppg_test_list.append(ppg_test)
     
     for dataset in datasets:
         
-        ecg_train = np.load(DATA_PATH + dataset + f"/lead1_train.npy", allow_pickle=True).reshape(-1, 128*window_size)
-        ppg_train = np.load(DATA_PATH + dataset + f"/lead3_train.npy", allow_pickle=True).reshape(-1, 128*window_size)
+        ecgall_train = np.load(DATA_PATH + dataset + f"/lead4_train.npy", allow_pickle=True).reshape(-1, 128*window_size)
+        ecgcond_train = np.load(DATA_PATH + dataset + f"/lead1_train.npy", allow_pickle=True).reshape(-1, 128*window_size)
         
-        ecg_test = np.load(DATA_PATH + dataset + f"/lead1_test.npy", allow_pickle=True).reshape(-1, 128*window_size)
-        ppg_test = np.load(DATA_PATH + dataset + f"/lead3_test.npy", allow_pickle=True).reshape(-1, 128*window_size)
+        ecgall_test = np.load(DATA_PATH + dataset + f"/lead4_test.npy", allow_pickle=True).reshape(-1, 128*window_size)
+        ecgcond_test = np.load(DATA_PATH + dataset + f"/lead1_test.npy", allow_pickle=True).reshape(-1, 128*window_size)
 
-        ecg_train_list.append(ecg_train)
-        ppg_train_list.append(ppg_train)
-        ecg_test_list.append(ecg_test)
-        ppg_test_list.append(ppg_test)
+        ecgall_train_list.append(ecgall_train)
+        ecgcond_train_list.append(ecgcond_train)
+        ecgall_test_list.append(ecgall_test)
+        ecgcond_test_list.append(ecgcond_test)
 
-    ecg_train = np.nan_to_num(np.concatenate(ecg_train_list).astype("float32"))
-    ppg_train = np.nan_to_num(np.concatenate(ppg_train_list).astype("float32"))
+    ecgall_train = np.nan_to_num(np.concatenate(ecgall_train_list).astype("float32"))
+    ecgcond_train = np.nan_to_num(np.concatenate(ecgcond_train_list).astype("float32"))
 
-    ecg_test = np.nan_to_num(np.concatenate(ecg_test_list).astype("float32"))
-    ppg_test = np.nan_to_num(np.concatenate(ppg_test_list).astype("float32"))
+    ecgall_test = np.nan_to_num(np.concatenate(ecgall_test_list).astype("float32"))
+    ecgcond_test = np.nan_to_num(np.concatenate(ecgcond_test_list).astype("float32"))
 
     dataset_train = ECGDataset(
-        skp.minmax_scale(ecg_train, (-1, 1), axis=1),
-        skp.minmax_scale(ppg_train, (-1, 1), axis=1)
+        skp.minmax_scale(ecgall_train, (-1, 1), axis=1),
+        skp.minmax_scale(ecgcond_train, (-1, 1), axis=1),
+        with_ecgclean=with_ecgclean
     )
     dataset_test = ECGDataset(
-        skp.minmax_scale(ecg_test, (-1, 1), axis=1),
-        skp.minmax_scale(ppg_test, (-1, 1), axis=1)
+        skp.minmax_scale(ecgall_test, (-1, 1), axis=1),
+        skp.minmax_scale(ecgcond_test, (-1, 1), axis=1),
+        with_ecgclean=with_ecgclean
     )
 
     return dataset_train, dataset_test
